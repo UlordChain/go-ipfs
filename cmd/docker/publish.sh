dd5676e10fe9b80b0acbc0f13587315134a250ad
danny <2528273456@qq.com>
2018-11-15 17:03:03 +0800

    fix docker publish name

--- cmd/docker/publish.sh
+++ cmd/docker/publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-cd ../ipfs
-GOOS=linux GOARCH=amd64 go build -o ../docker/ipfs_linux
+cd ../udfs
+GOOS=linux GOARCH=amd64 go build -o ../docker/udfs_linux
 
 cd ../docker
 
- IMAGE_NAME=\
+ IMAGE_NAME=dockerfordanny/udfs-test
  echo "start build image ${IMAGE_NAME}"
  docker build . -t ${IMAGE_NAME}
  echo "------------------------------------\n"
