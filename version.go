410256376201414c4d80ccc47bcd6c753f75922d
danny <2528273456@qq.com>
2019-01-28 09:02:08 +0800

    change version backup-v0.0.4

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package ipfs
 
 // CurrentCommit is the current git commit, this is set as a ldflag in the Makefile
-var CurrentCommit string = "feat/backup-v0.0.3"
+var CurrentCommit string = "feat/backup-v0.0.4"
 
 // CurrentVersionNumber is the current application's version literal
 const CurrentVersionNumber = "0.4.18"
